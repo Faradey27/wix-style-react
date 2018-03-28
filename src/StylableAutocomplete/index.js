bf5576f7d130e67476c381ebb195ff564dee6f8e
Amir Yonatan <amiry@wix.com>
2018-03-28 13:52:25 +0300

    re-added old autocomplete to support amaterial

--- src/StylableAutocomplete/index.js
+++ src/StylableAutocomplete/index.js
@@ -1,1 +1,16 @@
-export {Autocomplete} from 'wix-ui-backoffice/Autocomplete';
+import React from 'react';
+import {BOAutocomplete} from 'wix-ui-backoffice/Autocomplete';
+import {Autocomplete as OldAutocomplete} from '../DeprecatedAutoComplete';
+import {string} from 'prop-types';
+
+export const Autocomplete = props => {
+  if (props.theme === 'amaterial') {
+    return <OldAutocomplete {...props}/>;
+  }
+
+  return <BOAutocomplete {...props}/>;
+};
+
+Autocomplete.propTypes = {
+  theme: string
+};
