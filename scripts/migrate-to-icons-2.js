a2ec06008a62165114f0a69c0f5a5a56076242ed
Faradey27 <andriit@wix.com>
2018-05-31 14:39:48 +0300

    <Icons /> - added naive implementation of migration codemod

--- scripts/migrate-to-icons-2.js
+++ scripts/migrate-to-icons-2.js
@@ -12,4 +12,60 @@
   ran on http://electric-process.surge.sh/
 */
 const oldToNewIconsNamesMap = require('./assets/oldToNewIconsNamesMap.json');
-console.log(oldToNewIconsNamesMap);
+const oldIconNames = Object.keys(oldToNewIconsNamesMap);
+const getOldIconName = value => oldIconNames.find(iconName => value.includes(iconName));
+
+const RED = '\x1b[31m';
+const CYAN = '\x1b[36m';
+
+const errors = [];
+const migratedIcons = [];
+
+const printDepricationMessage = error => {
+  console.log(RED, error.text);
+  console.log(CYAN, 'Problem happen in file', error.where);
+  console.log(CYAN, 'On trying to migrate such value', error.fullValue);
+  console.log('\n');
+};
+
+const transformFile = (file, api) => {
+  const j = api.jscodeshift;
+  const root = j(file.source);
+  root
+    .find(j.ImportDeclaration)
+    .filter(node => getOldIconName(node.value.source.value) && node.value.source.value.includes('Icons'))
+    .forEach(node => {
+      const oldIconName = getOldIconName(node.value.source.value);
+      const newIconName = oldToNewIconsNamesMap[oldIconName];
+      if (!newIconName) {
+        errors.push({
+          text: `Icon with name "${oldIconName}" is not supported anymore, please ask your UX person to provide alternative`,
+          where: file.path,
+          fullValue: node.value.source.value
+        });
+      } else {
+        migratedIcons.push({
+          text: `Icon with name "${oldIconName}" was migrated, now it called "${newIconName}"`,
+          where: file.path,
+          fullValue: node.value.source.value
+        });
+        node.value.source.value = `wix-style-react/icons2/${newIconName}`;
+      }
+    });
+
+  return root.toSource({quote: 'single'});
+};
+
+module.exports = (file, api) => {
+  const result = transformFile(file, api);
+
+  console.log(CYAN, '\n', migratedIcons.length, 'icons was migrated \n');
+
+  if (errors.length) {
+    console.log(RED, '\n', errors.length, ' icons was not migrated and require your attention: \n');
+  }
+
+  errors.forEach(printDepricationMessage);
+
+  return result;
+};
